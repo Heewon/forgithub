bf9c9481d55f30439d62f4a63c74d536ca8dc8a4
희원 윤 <colorlime@gmail.com>
2011-12-27 01:10:00 +0900

    create new
    새로운 파일 만들었음~

---
+++ forgithub/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  forgithub
+//
+//  Created by 희원 윤 on 11. 12. 27..
+//  Copyright 2011 고려대학교. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    cout<< "wowwwowww";
+    return 0;
+}
+
